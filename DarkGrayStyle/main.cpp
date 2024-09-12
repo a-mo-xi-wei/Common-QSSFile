e1ecbaf9c858c71ffe2c07698064c8c8505f9544
WeiWang <1428206861@qq.com>
2024-09-12 08:39:58 +0800

    ok

---
+++ DarkGrayStyle/main.cpp
@@ -0,0 +1,23 @@
+/*
+###############################################################################
+#                                                                             #
+# The MIT License                                                             #
+#                                                                             #
+# Copyright (C) 2017 by Juergen Skrotzky (JorgenVikingGod@gmail.com)          #
+#               >> https://github.com/Jorgen-VikingGod                        #
+#                                                                             #
+# Sources: https://github.com/Jorgen-VikingGod/Qt-Frameless-Window-DarkStyle  #
+#                                                                             #
+###############################################################################
+*/
+
+#include <QApplication>
+#include "DarkStyle.h"
+
+int main(int argc, char *argv[]) {
+  QApplication a(argc, argv);
+
+  QApplication::setStyle(new DarkStyle);
+
+  return a.exec();
+}
